df31de45d45264ae159bbddd330aa29a7308433b
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2024-10-07 20:54:37 +0900

    Index関数をリファクタリングした

--- SImgProc/ImgProc.cpp
+++ SImgProc/ImgProc.cpp
@@ -486,117 +486,113 @@ BOOL Sort(double* arr, int iLength, double* brr)
 }
 
 
-BOOL Index(double* arr, int iLength, int* brr)
+BOOL Index(double* dIn, int iLength, int* iIndexOut)
 {
 	int* iIndex;
-	double* dOut;
-	dOut = new double [iLength];
+	double* dALocal;
+	dALocal = new double [iLength];
 	iIndex = new int [iLength];
 	
 
-	for(int ii=0; ii<iLength; ii++)
+	for(int i=0; i<iLength; i++)
 	{
-		dOut[ii]=arr[ii];
-		iIndex[ii]=ii;
+		dALocal[i]=dIn[i];
+		iIndex[i]=i;
 	}
 
 	int m=7;
 	int nstack=64;
-	int i, ir, j, k, jstack=-1, l=0, n=iLength;
-	double a, b;
+	int iIndexR, jstack=-1, indexL=0, n=iLength;
+	double a;
+	int b;
 	int* istack;
 	istack=new int[nstack];
-	ir=n-1;
-	for(;;)
+	iIndexR=n-1;
+
+	while(1)
 	{
-		if(ir-1<m)
+		if(iIndexR-1<m)
 		{
-			for(j=l+1; j<=ir; j++)
+			for(int j=indexL+1; j<=iIndexR; j++)
 			{
-				a=dOut[j];
+				a=dALocal[j];
 				b=iIndex[j];
-				for(i=j-1;i>=l;i--)
+				int i;
+				for(i=j-1; i>=indexL; i--)
 				{
-					if(dOut[i]<=a){break;}
-					dOut[i+1]=dOut[i];
+					if(dALocal[i]<=a){break;}
+					dALocal[i+1]=dALocal[i];
 					iIndex[i+1]=iIndex[i];
 				}
-				dOut[i+1]=a;
+				dALocal[i+1]=a;
 				iIndex[i+1]=b;
 			}
 			if(jstack<0){break;}
-			ir=istack[jstack--];
-			l=istack[jstack--];
+			iIndexR=istack[jstack--];
+			indexL=istack[jstack--];
+			continue;
 		}
-		else
-		{
-			k=(l+ir)>>1;
-			SWAP(&(dOut[l]),&(dOut[ir]));
-			SWAP(&(iIndex[l]),&(iIndex[ir]));
-		}
-		if(dOut[l] > dOut[ir])
-		{
-			SWAP(&(dOut[l]), &(dOut[ir]));
-			SWAP(&(iIndex[l]), &(iIndex[ir]));
-		}
-		if(dOut[l+1] > dOut[ir])
-		{
-			SWAP(&(dOut[l+1]), &(dOut[ir]));
-			SWAP(&(iIndex[l+1]), &(iIndex[ir]));
-		}
-		if(dOut[l] > dOut[l+1])
-		{
-			SWAP(&(dOut[l]), &(dOut[l+1]));
-			SWAP(&(iIndex[l]), &(iIndex[l+1]));
-		}
-		i=l+1;
-		j=ir;
-		a=dOut[l+1];
-		b=iIndex[l+1];
-		for(;;)
-		{
-			do
-			{
-				i++;
-			}while(dOut[i]<a);
 
-			do
-			{
-				j--;
-			}while(dOut[j]>a);
+		int iIndexTemp=(indexL+iIndexR)>>1;
+		SWAP(&(dALocal[iIndexTemp]),&(dALocal[indexL+1]));
+		SWAP(&(iIndex[iIndexTemp]),&(iIndex[indexL+1]));
+
+		if(dALocal[indexL] > dALocal[iIndexR])
+		{
+			SWAP(&(dALocal[indexL]), &(dALocal[iIndexR]));
+			SWAP(&(iIndex[indexL]), &(iIndex[iIndexR]));
+		}
+		if(dALocal[indexL+1] > dALocal[iIndexR])
+		{
+			SWAP(&(dALocal[indexL+1]), &(dALocal[iIndexR]));
+			SWAP(&(iIndex[indexL+1]), &(iIndex[iIndexR]));
+		}
+		if(dALocal[indexL] > dALocal[indexL+1])
+		{
+			SWAP(&(dALocal[indexL]), &(dALocal[indexL+1]));
+			SWAP(&(iIndex[indexL]), &(iIndex[indexL+1]));
+		}
+
+
+		int i=indexL+1;
+		int j=iIndexR;
+		a=dALocal[indexL+1];
+		b=iIndex[indexL+1];
+		while(1)
+		{
+			while(1){i++;if(dALocal[i]>=a){break;}}
+			while(1){j--;if(dALocal[j]<=a){break;}}
+
 			if(j<i){break;}
 
-			SWAP(&(dOut[i]), &(dOut[j]));
+			SWAP(&(dALocal[i]), &(dALocal[j]));
 			SWAP(&(iIndex[i]), &(iIndex[j]));
 		}
-		dOut[l+1]=dOut[j];
-		dOut[j]=a;
-		iIndex[l+1]=iIndex[j];
+		dALocal[indexL+1]=dALocal[j];
+		dALocal[j]=a;
+		iIndex[indexL+1]=iIndex[j];
 		iIndex[j]=a;
 		jstack += 2;
 
 		if(jstack>=nstack){return FALSE;}
-		if(ir-i+1 >= j-1)
+		if(iIndexR-i+1 >= j-1)
 		{
-			istack[jstack]=ir;
+			istack[jstack]=iIndexR;
 			istack[jstack-1]=i;
-			ir=j-1;
+			iIndexR=j-1;
 		}
 		else
 		{
 			istack[jstack]=j-1;
-			istack[jstack-1]=l;
-			l=i;
+			istack[jstack-1]=indexL;
+			indexL=i;
 		}
 	}
 
-	for(int ii=0; ii<iLength; ii++)
-	{
-		brr[ii]=iIndex[ii];
-	}
+	for(int i=0; i<iLength; i++){iIndexOut[i]=iIndex[i];}
 
 	delete [] iIndex;
-	delete [] dOut;
+	delete [] dALocal;
 	return TRUE;
 }
 
